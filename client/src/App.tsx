2cf9d2bd92fc3a94c1931712a8d7f67a6e97af06
Vikash Gupta <vikasharmy811@gmail.com>
2026-03-26 18:14:19 +0530

    login page added to landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
 import './App.css'
+import { Routes, Route, Link } from 'react-router-dom'
+import LoginPage from './login/login_page/src/App'
 
 function App() {
   return (
-    <main className="w-full h-full flex flex-col items-center justify-center">
-      <h1 className="text-white text-8xl font-bold tracking-tighter">
-        ARYNOX
-      </h1>
-    </main>
+    <Routes>
+      <Route path="/" element={
+        <div className="flex flex-col h-screen w-full items-center justify-center text-white bg-[#111]">
+          <h1 className="text-4xl font-bold mb-4">Welcome to ARYNOX</h1>
+          <Link to="/login" className="text-[#7C3AED] hover:underline">Go to Login Page</Link>
+        </div>
+      } />
+      <Route path="/login" element={<LoginPage />} />
+    </Routes>
   )
 }
 
